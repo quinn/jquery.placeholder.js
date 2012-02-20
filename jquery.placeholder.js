ec73554763a9e02d5410bdc47c145bc468ff17ca
Quinn Shanahan <q.shanahan+service@gmail.com>
2012-02-20 16:26:45 -0500

    updates

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -1,43 +1,50 @@
-function supportsInputPlaceholder() {
+jQuery.supportsInputPlaceholder = function() {
   var i = document.createElement('input');
   return 'placeholder' in i;
 }
 
+jQuery.insertPlaceholder = function () {
+  var el = $(this);
+  var text = el.attr('placeholder');
+
+  if (el.val() == "" || el.val() == text) {
+    el.val(text);
+    el.addClass('placeholding');
+  }
+  else {
+    el.removeClass('placeholding');
+  }
+}
+
+jQuery.clearPlaceholder = function () {
+  var el = $(this);
+  var text = el.attr('placeholder');
+
+  el.removeClass('placeholding')
+  if (el.val() == text) {
+    el.val('');
+  }
+}
+
 jQuery.fn.placeholder = function () {
-  var insertPlaceholder = function () {
-    var el = $(this);
-    var text = el.attr('placeholder');
-
-    if (el.val() == "" || el.val() == text) {
-      el.val(text);
-      el.addClass('placeholding');
-    }
-    else {
-      el.removeClass('placeholding');
-    }
-  }
-
-  var clearPlaceholder = function () {
-    var el = $(this);
-    var text = el.attr('placeholder');
-
-    el.removeClass('placeholding')
-    if (el.val() == text) {
-      el.val('');
-    }
-  }
 
   this
-    .live('blur', insertPlaceholder)
-    .live('change', insertPlaceholder)
-    .live('focus', clearPlaceholder)
-    .each(insertPlaceholder);
+    .live('blur', jQuery.insertPlaceholder)
+    .live('change', jQuery.insertPlaceholder)
+    .live('focus', jQuery.clearPlaceholder)
+    .each(jQuery.insertPlaceholder);
+
   return this;
 };
 
-
 jQuery(function ($) {
-  if ( $(!supportsInputPlaceholder()) ) {
-    $('input[placeholder]').placeholder();
+  if ( !jQuery.supportsInputPlaceholder() ) {
+    $('form').live('submit', function () {
+      $(this).find('input[placeholder], textarea[placeholder]').each(function () {
+        jQuery.clearPlaceholder.apply(this);
+      });
+    });
+
+    $('input[placeholder], textarea[placeholder]').placeholder();
   }
 });
